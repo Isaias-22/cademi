eda6ccb87aa6b5b5bb17a5754e5cafa358a7698e
PabloFdezm <pablo.fernandezmo@mail.udp.cl>
2020-08-20 23:34:39 -0400

    New codes

--- code.R
+++ code.R
@@ -4,6 +4,9 @@ library(ggplot2)
 library(tidyr)
 library(ggthemes)
 
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(hrbrthemes, gganimate)
+
 data <- getURL("https://raw.githubusercontent.com/PabloFdezm/cademi/master/data.csv?token=AKX6XZP5VWIKIMLSZYTNRV27JBDCS")
 data <- read.csv(text= data, row.names = 1)
 
@@ -28,9 +31,41 @@ data %>%
   geom_point(aes(x=detail,y=mean,color="Promedio"))+
   geom_point(aes(x=detail,y=median,color="Mediana"))+
   scale_y_continuous(limits = c(0,4))+
-  theme(axis.text.x = element_text(angle=90))+
   labs(title="Promedio vs Mediana",
        x="",
        y="Promedio",
        colour=" ")+
-  theme_stata()
+  hrbrthemes::theme_ipsum()+
+  theme(axis.text.x = element_text(angle=10))
+  
+data %>%
+  select(equipoID, starts_with("pe"))%>%
+  pivot_longer(-equipoID, names_to = "detail", values_to = "value")%>%
+  ggplot()+
+  geom_density(aes(x=value, fill=detail),alpha=0.3)+
+  labs(title = "Density by Performance",
+       x=" ",
+       y="Density",
+         fill="Fill:")+
+  hrbrthemes::theme_ipsum()
+
+data %>%
+  select(equipoID, ends_with("1"))%>%
+  pivot_longer(-equipoID, names_to = "detail", values_to = "value")%>%
+  ggplot()+
+  geom_density(aes(x=value, fill=detail))+
+  facet_wrap(~ detail)+
+  hrbrthemes::theme_tinyhand()+
+  labs(title = "Density Facet by Detail")
+
+data %>%
+  select(equipoID, starts_with("psy"))%>%
+  pivot_longer(-equipoID, names_to = "detail", values_to = "value")%>%
+  group_by(detail)%>%
+  summarize(mean = mean(value))%>%
+  ggplot(aes(x=detail, y=mean))+
+  geom_col()+
+  labs(title="Promedio de Seguridad Psicologica",
+       x="",
+       y="Promedio")+
+  hrbrthemes::theme_ipsum()
